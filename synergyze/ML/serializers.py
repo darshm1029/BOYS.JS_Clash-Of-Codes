647ac5c698629b2b7d60858c40f39310f65aad57
Deadpool <abcd162003@gmail.com>
2023-03-05 07:46:39 +0530

    swagger support for image api's

--- synergyze/ML/serializers.py
+++ synergyze/ML/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 
 class ImgSerializer(serializers.Serializer):
     image = serializers.ImageField()
+
+
+class VerifySerializer(serializers.Serializer):
+    image1 = serializers.ImageField()
+    image2 = serializers.ImageField()
